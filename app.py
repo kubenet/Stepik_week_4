9cec7b710fce5fa480a8d196b8e049186f0f2851
kubenet <kubenet@gmail.com>
2020-07-28 15:04:24 +0700

    upload data in database from data.json

--- app.py
+++ app.py
@@ -270,34 +270,5 @@ def booking_done():
                            clientTime=client_time, clientTeacher=client_teacher, clientWeekday=client_weekday)
 
 
-def import_json_data():
-    with open('data.json', 'r') as r:
-        all_data = json.load(r)
-        for teacher in all_data[1]:
-            t = Teachers(
-                name=teacher['name'],
-                about=teacher['about'],
-                rating=teacher['rating'],
-                picture=teacher['picture'],
-                price=teacher['price'],
-                lesson_time='8:00')
-            db.session.add(t)
-            for goal in teacher['goals']:
-                db.session.add(Goals(key=goal, goal=t))
-            for day in teacher['free']:
-                for times, status in teacher['free'][day].items():
-                    db.session.add(
-                        TimetableTeachers(
-                            day_times=str(times),
-                            status=status,
-                            week=t
-                        )
-                    )
-    db.session.commit()
-    r.close()
-
-
-import_json_data()
-
 if __name__ == "__main__":
     app.run('0.0.0.0', debug=True)
