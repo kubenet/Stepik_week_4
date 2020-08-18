5235c1e4cc48e42265dbd974050aff8ee96fcf47
kubenet <kubenet@gmail.com>
2020-08-18 15:06:45 +0700

    add record in table db from booking.html

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-import json
-import hashlib
 from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
 from wtforms import RadioField, StringField
@@ -7,19 +5,13 @@ from wtforms.validators import InputRequired, Length
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
-with open('data.json', 'r') as r:
-    all_data = json.load(r)
-    md5Hash = hashlib.md5(str(all_data).encode())
-    md5Hashed = md5Hash.hexdigest()
-    r.close()
-
 app = Flask(__name__)
 CSRF_ENABLED = True
-SECRET_KEY = md5Hashed
-app.secret_key = md5Hashed
+SECRET_KEY = 'b706835de79a2b4e80506f582af3676a'
+app.secret_key = 'b706835de79a2b4e80506f582af3676a'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///base.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-WTF_CSRF_SECRET_KEY = md5Hashed
+WTF_CSRF_SECRET_KEY = 'b706835de79a2b4e80506f582af3676a'
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
@@ -91,108 +83,11 @@ class Booking(db.Model):
     day_times = db.relationship("TimetableTeachers", back_populates="booking", uselist=False)
 
 
-# db.drop_all()
-# db.create_all()
-# teacher1 = Teachers(name='Ivan', about='about', rating=4.5, price=900, lesson_time='1')
-# teacher2 = Teachers(name='Fedor', about='about2', rating=4, price=9, lesson_time='13')
-# teacher3 = Teachers(name='Vasya', about='about3', rating=5, price=90, lesson_time='41')
-#
-# b = Booking(client_name='Semyon', client_phone='4440009993322')
-# t1 = TimetableTeachers(day_times="8:00", status=False, booking=b, week=teacher3)
-#
-# search_teacher = SearchTeacher(how_time='1-2 часа', client_name='Igor', client_phone='79993332211')
-#
-# week = TimetableTeachers(day_times="8:00", status=False, week=teacher1)
-# week1 = TimetableTeachers(day_times="10:00", status=True, week=teacher1)
-# week2 = TimetableTeachers(day_times="12:00", status=True, week=teacher1)
-# week3 = TimetableTeachers(day_times="14:00", status=True, week=teacher1)
-# week4 = TimetableTeachers(day_times="16:00", status=True, week=teacher3)
-# week5 = TimetableTeachers(day_times="18:00", status=True, week=teacher3)
-# week6 = TimetableTeachers(day_times="20:00", status=True, week=teacher3)
-# week7 = TimetableTeachers(day_times="22:00", status=True, week=teacher3)
-# week8 = TimetableTeachers(day_times="21:00", status=True, week=teacher3)
-#
-# goal1 = Goals(key='fly', goal=teacher1)
-# goal2 = Goals(key='learn', goal=teacher1)
-# goal3 = Goals(key='travel', goal=teacher1)
-# goal4 = Goals(key='travel', goal=teacher3)
-# goal_s = Goals(key='travel', search_teacher=search_teacher)
-#
-# db.session.add(goal1)
-# db.session.add(goal2)
-# db.session.add(goal3)
-# db.session.add(goal4)
-# db.session.add(search_teacher)
-#
-# db.session.add(b)
-# db.session.add(t1)
-#
-# db.session.add(week)
-# db.session.add(week1)
-# db.session.add(week2)
-# db.session.add(week3)
-# db.session.add(week4)
-# db.session.add(week5)
-# db.session.add(week6)
-# db.session.add(week7)
-# db.session.add(week8)
-# db.session.add(teacher1)
-# db.session.add(teacher2)
-# db.session.add(teacher3)
-#
-# db.session.commit()
-# t1 = Teachers.query.get(1)
-# print(t1.name)
-# print(t1.about)
-# print(t1.rating)
-# print(t1.price)
-# print(t1.lesson_time)
-# print(t1.goals[0].key)
-# print(t1.goals[1].key)
-# print(t1.goals[2].key)
-
-
 def add_record(name, about, rating, price, goal, lesson_time):
     teacher = Teachers(name=name, about=about, rating=rating, price=price, goal=goal, lesson_time=lesson_time)
     db.session.add(teacher)
     db.session.commit()
     return teacher.id  # id нового преподавателя в БД
-
-
-# Запись нового запроса в файл all_requests.json
-def add_request(name, phone, goal, times):
-    with open('requests.json', 'r') as read_json:
-        records = json.load(read_json)
-    records.append({'name': name, 'phone': phone, 'goal': goal, 'times': times})
-    read_json.close()
-    with open('requests.json', 'w') as write_json:
-        json.dump(records, write_json)
-        write_json.close()
-
-
-# Запись нового запроса в файл all_requests.json
-def update_timetable_teacher(id_teacher, day, times, client_name, client_phone):
-    with open('data.json', 'r') as read_json:
-        records = json.load(read_json)
-        records[1][int(id_teacher)]['free'][day][times] = False
-        read_json.close()
-
-    with open('data.json', 'w') as w:
-        json.dump(records, w)
-        w.close()
-
-    with open('data.json', 'r') as r:
-        global all_data
-        all_data = json.load(r)
-        r.close()
-
-    with open('booking.json', 'r') as read_booking:
-        records = json.load(read_booking)
-        records.append([id_teacher, day, times, client_name, client_phone])
-        read_booking.close()
-        with open('booking.json', 'w') as write_booking:
-            json.dump(records, write_booking)
-            write_booking.close()
 
 
 class RequestForm(FlaskForm):  # объявление класса формы для WTForms
@@ -238,13 +133,13 @@ for g in goal:
 def index():
     teachers_query = db.session.query(Teachers).order_by(Teachers.rating).limit(6)
     global list_goal
-    return render_template("index.html", all_data=all_data, goals=list_goal, teachers=teachers_query.all())
+    return render_template("index.html", goals=list_goal, teachers=teachers_query.all())
 
 
 @app.route('/teachers/')  # все репетиторы
 def teachers():
     teachers_query = db.session.query(Teachers).order_by(Teachers.rating)
-    return render_template("teachers.html", all_data=all_data, goals=query_goals(), teachers=teachers_query.all())
+    return render_template("teachers.html", goals=query_goals(), teachers=teachers_query.all())
 
 
 @app.route('/goals/<goal>/')  # цель 'goal'
@@ -279,6 +174,7 @@ def request_teacher():
             client_name=form.name.data,
             client_phone=form.phone.data,
         )
+
         goal = Goals(
             key=goal_choices[form.goal.data],
             search_teacher=teacher
@@ -295,15 +191,14 @@ def request_teacher():
                                goal=goal_choices[form.goal.data], time=time_choices[form.time.data])
 
     # Обработка запроса GET
-    return render_template("request.html", form=form, all_data=all_data)
+    return render_template("request.html", form=form)
 
 
 @app.route('/booking/<int:id_teacher>/<day>/<time>/',
            methods=['GET', 'POST'])  # здесь будет форма бронирования <id учителя>
 def booking(id_teacher, day, time):
-    form = RequestForm()
     # Прием данных из формы
-    if request.method == "POST" and form.validate():
+    if request.method == "POST":
         # получаем даныне из формы
         client_weekday = request.form["clientWeekday"]
         client_time = request.form["clientTime"]
@@ -311,17 +206,25 @@ def booking(id_teacher, day, time):
         client_name = request.form["clientName"]
         client_phone = request.form["clientPhone"]
 
-        # Обновляем расписание свободного времени репетитора
-        update_timetable_teacher(teacher, client_weekday, client_time, client_name, client_phone)
+        # записываем выбранное время репетитора
+        booking_teacher = Booking(
+            client_name=client_name,
+            client_phone=client_phone
+        )
+
+        db.session.add(booking_teacher)
+        try:
+            db.session.commit()
+        except:
+            return render_template("404.html")
 
         return render_template("booking_done.html", clientName=client_name, clientPhone=client_phone,
-                               clientTime=client_time, clientTeacher=teacher, clientWeekday=client_weekday)
-    else:
-        render_template('404.html')
+                               time=client_time, clientTeacher=teacher, day=client_weekday)
+
     # Обработка запроса GET
     teacher = db.session.query(Teachers).get_or_404(id_teacher)
 
-    return render_template("booking.html", id_techers=teacher.id, day=day, time=time, all_data=all_data)
+    return render_template("booking.html", teacher=teacher, day=day, time=time)
 
 
 if __name__ == "__main__":
